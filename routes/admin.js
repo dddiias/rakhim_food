9672ea559a19dc74c855777dc44cffde198e10c2
Dias Salakhutdinov <220182@astanait.edu.kz>
2024-02-28 07:30:00 +0600

    protected admin panel

--- routes/admin.js
+++ routes/admin.js
@@ -2,29 +2,30 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 const Order = require('../models/Order');
+const requireAdminAuth = require('../middleware/AdminAuth');
 
-router.get('/admin/menu', async (req, res) => {
+router.get('/admin/menu', requireAdminAuth, async (req, res) => {
     const products = await Product.find();
     res.render('admin/adminMenu', { products });
 });
 
-router.get('/admin/menu/new', (req, res) => {
+router.get('/admin/menu/new', requireAdminAuth, (req, res) => {
     res.render('admin/addProduct');
 });
 
-router.post('/admin/menu', async (req, res) => {
+router.post('/admin/menu', requireAdminAuth, async (req, res) => {
     const { name, description, price, images } = req.body;
     const newProduct = new Product({ name, description, price, images });
     await newProduct.save();
     res.redirect('/admin/menu');
 });
 
-router.post('/admin/menu/delete/:id', async (req, res) => {
+router.post('/admin/menu/delete/:id', requireAdminAuth, async (req, res) => {
     await Product.findByIdAndDelete(req.params.id);
     res.redirect('/admin/menu');
 });
 
-router.get('/admin/products/edit/:id', async (req, res) => {
+router.get('/admin/products/edit/:id', requireAdminAuth, async (req, res) => {
     console.log("Editing product with ID:", req.params.id);
     try {
         const product = await Product.findById(req.params.id);
@@ -35,7 +36,7 @@ router.get('/admin/products/edit/:id', async (req, res) => {
     }
 });
 
-router.post('/admin/products/update/:id', async (req, res) => {
+router.post('/admin/products/update/:id', requireAdminAuth, async (req, res) => {
     const { name, description, price, images } = req.body;
     try {
         await Product.findByIdAndUpdate(req.params.id, {
@@ -51,7 +52,7 @@ router.post('/admin/products/update/:id', async (req, res) => {
     }
 });
 
-router.get('/admin/orders', async (req, res) => {
+router.get('/admin/orders', requireAdminAuth, async (req, res) => {
     try {
         const sortDirection = req.query.sort === 'desc' ? -1 : 1;
         const orders = await Order.find().sort({ total: sortDirection });
@@ -63,7 +64,7 @@ router.get('/admin/orders', async (req, res) => {
 });
 
 
-router.get('/admin/orders/:orderId', async (req, res) => {
+router.get('/admin/orders/:orderId', requireAdminAuth, async (req, res) => {
     try {
         const orderId = req.params.orderId;
         const order = await Order.findById(orderId);
@@ -79,7 +80,7 @@ router.get('/admin/orders/:orderId', async (req, res) => {
     }
 });
 
-router.post('/admin/orders/:orderId/update-status', async (req, res) => {
+router.post('/admin/orders/:orderId/update-status', requireAdminAuth, async (req, res) => {
     try {
         const orderId = req.params.orderId;
         const newStatus = req.body.status;
@@ -97,7 +98,7 @@ router.post('/admin/orders/:orderId/update-status', async (req, res) => {
     }
 });
 
-router.get('/admin/ordersByDay', async (req, res) => {
+router.get('/admin/ordersByDay', requireAdminAuth, async (req, res) => {
     try {
         let query = {};
         const { startDate, endDate } = req.query;
@@ -117,8 +118,29 @@ router.get('/admin/ordersByDay', async (req, res) => {
 });
 
 
+router.get('/admin', (req, res) => {
+    res.render('admin/admin'); 
+});
 
+router.post('/admin/login', (req, res) => {
+    const { username, password } = req.body;
 
+    if (username === 'admin' && password === 'admin') {
+        req.session.adminLoggedIn = true; 
+        res.redirect('/admin/dashboard'); 
+    } else {
+        res.render('admin/admin', { error: 'Неверный логин или пароль' }); 
+    }
+});
+
+router.get('/admin/dashboard', requireAdminAuth, (req, res) => {
+    res.render('admin/dashboard'); 
+});
+
+router.get('/admin/logout', (req, res) => {
+    req.session.adminLoggedIn = false; 
+    res.redirect('/admin/login'); 
+});
 
 
 module.exports = router;
